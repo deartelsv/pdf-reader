86a83e904c7b5af5181b86c8e2de1eec055e90ae
artelsv <s.druzhinin@it-cron.ru>
2023-09-27 15:24:35 +0200

    add publishing

--- pdfreader/build.gradle.kts
+++ pdfreader/build.gradle.kts
@@ -1,8 +1,8 @@
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.com.android.library)
     alias(libs.plugins.org.jetbrains.kotlin.android)
-    id("maven-publish")
+//    id("maven-publish")
 }
 
 android {
@@ -10,29 +10,29 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "de.artelsv.pdfreader"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        version = "1.0.0"
+//        applicationId = "de.artelsv.pdfreader"
+//        minSdk = 21
+//        targetSdk = 33
+//        versionCode = 1
+//        version = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-            signingConfig = signingConfigs.getByName("release")
-        }
-
-        getByName("debug") {
-            isDebuggable = true
-        }
-    }
+//    buildTypes {
+//        getByName("release") {
+//            isMinifyEnabled = false
+//            proguardFiles(
+//                getDefaultProguardFile("proguard-android-optimize.txt"),
+//                "proguard-rules.pro"
+//            )
+//            signingConfig = signingConfigs.getByName("release")
+//        }
+//
+//        getByName("debug") {
+//            isDebuggable = true
+//        }
+//    }
 
     buildFeatures {
         viewBinding = true
@@ -64,16 +64,16 @@ dependencies {
     implementation(libs.androidx.appcompat)
 }
 
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "de.artelsv"
-            artifactId = "pdf-reader"
-            version = "1.0.0"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-}
+//publishing {
+//    publications {
+//        register<MavenPublication>("release") {
+//            groupId = "de.artelsv"
+//            artifactId = "pdf-reader"
+//            version = "1.0.0"
+//
+//            afterEvaluate {
+//                from(components["release"])
+//            }
+//        }
+//    }
+//}
